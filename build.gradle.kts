a161ceaaeb450d11cd378c3d965ac02bffc095ba
dordsor21 <dordsor21@gmail.com>
2021-06-20 20:37:05 +0100

    Update to v6/1.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
@@ -8,12 +7,13 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = sourceCompatibility
+plugins.withId("java") {
+    the<JavaPluginExtension>().toolchain {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    }
 }
 
-version = "5.1.1"
+version = "6.0.0"
 
 repositories {
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
@@ -26,11 +26,12 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.plotsquared:PlotSquared-Core:5.13.11")
-    compileOnlyApi("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.6")
+    compileOnly("com.plotsquared:PlotSquared-Bukkit:6.0.0-SNAPSHOT")
+    compileOnly("com.plotsquared:PlotSquared-Core:6.0.0-SNAPSHOT")
+    compileOnlyApi("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
+    compileOnly("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.7")
     compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0")
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.5")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.0-SNAPSHOT")
     implementation("org.bstats:bstats-bukkit:2.2.1")
     implementation("org.bstats:bstats-base:2.2.1")
 }
@@ -38,7 +39,7 @@ dependencies {
 bukkit {
     name = "HoloPlots"
     main = "com.empcraft.holoplots.Main"
-    authors = listOf("Empire92", "NotMyFault")
+    authors = listOf("Empire92", "NotMyFault", "dordsor21")
     apiVersion = "1.13"
     description = "Holographic Plot signs"
     version = rootProject.version.toString()
--- src/main/java/com/empcraft/holoplots/PSHoloUtil.java
+++ src/main/java/com/empcraft/holoplots/PSHoloUtil.java
@@ -3,7 +3,9 @@ package com.empcraft.holoplots;
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
 import com.plotsquared.core.PlotSquared;
-import com.plotsquared.core.configuration.Captions;
+import com.plotsquared.core.configuration.caption.Caption;
+import com.plotsquared.core.configuration.caption.LocaleHolder;
+import com.plotsquared.core.configuration.caption.TranslatableCaption;
 import com.plotsquared.core.generator.GridPlotWorld;
 import com.plotsquared.core.location.Location;
 import com.plotsquared.core.plot.Plot;
@@ -11,14 +13,19 @@ import com.plotsquared.core.plot.PlotArea;
 import com.plotsquared.core.util.task.TaskManager;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.Template;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
-import java.util.Set;
 
 public class PSHoloUtil implements IHoloUtil {
+
+    public static final LegacyComponentSerializer LEGACY_COMPONENT_SERIALIZER = LegacyComponentSerializer.legacySection();
+    public static final MiniMessage MINI_MESSAGE = MiniMessage.builder().build();
 
     public static HashMap<Plot, Hologram> holograms = new HashMap<>();
 
@@ -30,8 +37,8 @@ public class PSHoloUtil implements IHoloUtil {
         BlockVector3 pos1 = BlockVector3.at(bx - 1, 0, bz - 1);
         BlockVector3 pos2 = BlockVector3.at(bx + 16, 255, bz + 16);
         CuboidRegion region = new CuboidRegion(pos1, pos2);
-        Set<PlotArea> areas = PlotSquared.get().getPlotAreas(world, region);
-        if (areas.size() == 0) {
+        PlotArea[] areas = PlotSquared.get().getPlotAreaManager().getPlotAreas(world, region);
+        if (areas.length == 0) {
             return;
         }
         for (PlotArea area : areas) {
@@ -39,9 +46,9 @@ public class PSHoloUtil implements IHoloUtil {
                 continue;
             }
             GridPlotWorld gpw = (GridPlotWorld) area;
-            Plot plot = gpw.getOwnedPlotAbs(new Location(area.getWorldName(), bx, 0, bz + 1));
+            Plot plot = gpw.getOwnedPlotAbs(Location.at(area.getWorldName(), BlockVector3.at(bx, 0, bz + 1), 0, 0));
             if (plot == null) {
-                plot = gpw.getOwnedPlotAbs(new Location(area.getWorldName(), pos2.getX(), 0, pos2.getZ() + 1));
+                plot = gpw.getOwnedPlotAbs(Location.at(area.getWorldName(), BlockVector3.at(pos2.getX(), 0, pos2.getZ() + 1), 0, 0));
             }
             if (plot == null || !plot.isBasePlot()) {
                 continue;
@@ -65,12 +72,12 @@ public class PSHoloUtil implements IHoloUtil {
 
                 // Call translate async as it might do HTTP requests
                 final Plot finalPlot = plot;
-                TaskManager.IMP.taskAsync(() -> {
-                    String line1 = translate(finalPlot, Captions.OWNER_SIGN_LINE_1.getTranslated());
-                    String line2 = translate(finalPlot, Captions.OWNER_SIGN_LINE_2.getTranslated());
-                    String line3 = translate(finalPlot, Captions.OWNER_SIGN_LINE_3.getTranslated());
-                    String line4 = translate(finalPlot, Captions.OWNER_SIGN_LINE_4.getTranslated());
-                    TaskManager.IMP.task(() -> {
+                TaskManager.getPlatformImplementation().taskAsync(() -> {
+                    String line1 = translate(finalPlot, TranslatableCaption.of("signs.owner_sign_line_1"));
+                    String line2 = translate(finalPlot, TranslatableCaption.of("signs.owner_sign_line_2"));
+                    String line3 = translate(finalPlot, TranslatableCaption.of("signs.owner_sign_line_3"));
+                    String line4 = translate(finalPlot, TranslatableCaption.of("signs.owner_sign_line_4"));
+                    TaskManager.getPlatformImplementation().task(() -> {
                         hologram.clearLines();
                         hologram.appendTextLine(line1);
                         hologram.appendTextLine(line2);
@@ -83,7 +90,7 @@ public class PSHoloUtil implements IHoloUtil {
         }
     }
 
-    private String translate(Plot plot, String string) {
+    private String translate(Plot plot, Caption caption) {
         String id = plot.getId().toString();
         String name;
         if (plot.getOwnerAbs() == null) {
@@ -94,7 +101,7 @@ public class PSHoloUtil implements IHoloUtil {
         if (name == null) {
             name = "unknown";
         }
-        return ChatColor.translateAlternateColorCodes('&', string.replaceAll("%id%", id)
-                .replaceAll("%plr%", name).replace("Claimed", plot.getOwners() == null ? "" : "Claimed"));
+        return LEGACY_COMPONENT_SERIALIZER.serialize(MINI_MESSAGE.parse(caption.getComponent(LocaleHolder.console()), Template.of("id", id),
+            Template.of("owner", name))).replace("Claimed", plot.getOwnerAbs() == null ? "" : "Claimed");
     }
 }
