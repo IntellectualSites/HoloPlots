1b753351ae6659c15b8455a7101ee9797c088b6f
dordsor21 <dordsor21@gmail.com>
2021-08-23 23:56:27 +0100

    Greatly improve how the plugin works - Delete holograms when plots are delete, also check for hologram viability when chunk is sent to player - Only check holograms if the chunk is in the correct corner of the plot

--- src/main/java/com/plotsquared/holoplots/HoloPlotsPlugin.java
+++ src/main/java/com/plotsquared/holoplots/HoloPlotsPlugin.java
@@ -1,7 +1,13 @@
 package com.plotsquared.holoplots;
 
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.google.common.eventbus.Subscribe;
+import com.plotsquared.core.PlotAPI;
+import com.plotsquared.core.events.PlotDeleteEvent;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 public class HoloPlotsPlugin extends JavaPlugin {
 
@@ -16,6 +22,7 @@ public class HoloPlotsPlugin extends JavaPlugin {
         HOLO = new PSHoloUtil();
         // Enable metrics
         new Metrics(this, BSTATS_ID);
+        new PlotAPI().registerListener(HOLO);
     }
 
 }
--- src/main/java/com/plotsquared/holoplots/PSHoloUtil.java
+++ src/main/java/com/plotsquared/holoplots/PSHoloUtil.java
@@ -2,16 +2,22 @@ package com.plotsquared.holoplots;
 
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import com.google.common.eventbus.Subscribe;
 import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.PlotSquared;
 import com.plotsquared.core.configuration.caption.Caption;
 import com.plotsquared.core.configuration.caption.LocaleHolder;
 import com.plotsquared.core.configuration.caption.TranslatableCaption;
+import com.plotsquared.core.events.PlayerClaimPlotEvent;
+import com.plotsquared.core.events.PlotDeleteEvent;
+import com.plotsquared.core.events.PlotMergeEvent;
 import com.plotsquared.core.generator.GridPlotWorld;
 import com.plotsquared.core.location.Location;
 import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.PlotArea;
+import com.plotsquared.core.plot.PlotId;
 import com.plotsquared.core.util.task.TaskManager;
+import com.plotsquared.core.util.task.TaskTime;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import net.kyori.adventure.text.minimessage.Template;
@@ -19,10 +25,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
 
 public class PSHoloUtil implements IHoloUtil {
 
-    public static HashMap<Plot, Hologram> holograms = new HashMap<>();
+    public static HashMap<HoloPlotID, Hologram> holograms = new HashMap<>();
 
     @Override
     public void updatePlayer(Player player, ChunkWrapper chunkWrapper) {
@@ -36,20 +45,23 @@ public class PSHoloUtil implements IHoloUtil {
         if (areas.length == 0) {
             return;
         }
+        areas:
         for (PlotArea area : areas) {
             if (!(area instanceof GridPlotWorld gridPlotWorld)) {
                 continue;
             }
-            Plot plot = gridPlotWorld.getOwnedPlotAbs(Location.at(area.getWorldName(), BlockVector3.at(bx, 0, bz + 1), 0, 0));
-            if (plot == null) {
-                plot = gridPlotWorld.getOwnedPlotAbs(Location.at(
-                        area.getWorldName(),
-                        BlockVector3.at(pos2.getX(), 0, pos2.getZ() + 1),
-                        0,
-                        0
-                ));
+            PlotId bot = gridPlotWorld.getPlotManager().getPlotIdAbs(bx, 0, bz);
+            PlotId top = gridPlotWorld.getPlotManager().getPlotIdAbs(bx + 15, 0, bz + 15);
+            if (top == null) { //Top corner of plot, or assume entirely road
+                continue;
+            } else if (top.equals(bot)) {
+                PlotId id = gridPlotWorld.getPlotManager().getPlotIdAbs(bx - 2, 0, bz - 2); //account for wall
+                if (id != null && id.equals(bot)) { //Plot entirely contains chunk
+                    continue;
+                }
             }
-            if (plot == null || !plot.isBasePlot()) {
+            Plot plot = gridPlotWorld.getPlotAbs(top);
+            if (plot == null) {
                 continue;
             }
             Location sign = area.getPlotManager().getSignLoc(plot);
@@ -59,14 +71,35 @@ public class PSHoloUtil implements IHoloUtil {
 
             org.bukkit.Location loc;
             if (x > pos1.getX() && x < pos2.getX() && z > pos1.getZ() && z < pos2.getZ()) {
+                if (!plot.hasOwner() || !plot.isBasePlot()) {
+                    Iterator<Map.Entry<HoloPlotID, Hologram>> it = holograms.entrySet().iterator();
+                    while (it.hasNext()) {
+                        Map.Entry<HoloPlotID, Hologram> entry = it.next();
+                        if (entry.getKey().getId().equals(top)) {
+                            entry.getValue().delete();
+                            it.remove();
+                            break areas;
+                        }
+                    }
+                    continue;
+                }
+
                 loc = new org.bukkit.Location(player.getWorld(), x + 0.5, sign.getY() + 3, z + 0.5);
 
+                HoloPlotID id = new HoloPlotID(plot.getId(), plot.getOwnerAbs());
+
                 final Hologram hologram;
-                if (!holograms.containsKey(plot)) {
+                if (!holograms.containsKey(id)) {
                     hologram = HologramsAPI.createHologram(HoloPlotsPlugin.THIS, loc);
-                    holograms.put(plot, hologram);
+                    holograms.put(id, hologram);
                 } else {
-                    hologram = holograms.get(plot);
+                    Hologram holo = holograms.get(id);
+                    if (holo.isDeleted()) {
+                        hologram = HologramsAPI.createHologram(HoloPlotsPlugin.THIS, loc);
+                        holograms.replace(id, hologram);
+                    } else {
+                        hologram = holo;
+                    }
                 }
 
                 // Call translate async as it might do HTTP requests
@@ -86,6 +119,48 @@ public class PSHoloUtil implements IHoloUtil {
                     });
                 });
             }
+        }
+    }
+
+    @Subscribe
+    public void onPlotClaim(PlayerClaimPlotEvent e) {
+        final Plot plot = e.getPlot();
+        final UUID uuid = e.getPlotPlayer().getUUID();
+        TaskManager.runTaskLater(() -> {
+            Player p = Bukkit.getPlayer(uuid);
+            if (p == null) {
+                return;
+            }
+            Location loc = plot.getArea().getPlotManager().getSignLoc(plot);
+            updatePlayer(p, new ChunkWrapper(loc.getX() >> 4, loc.getZ() >> 4, loc.getWorldName()));
+        }, TaskTime.ticks(20));
+    }
+
+    @Subscribe
+    public void onPlotDelete(PlotDeleteEvent e) {
+        UUID owner = e.getPlot().getOwnerAbs();
+        if (owner == null) {
+            return;
+        }
+        HoloPlotID id = new HoloPlotID(e.getPlotId(), owner);
+        Hologram hologram = PSHoloUtil.holograms.remove(id);
+        if (hologram != null) {
+            hologram.delete();
+        }
+    }
+
+    // Go overboard with removing holograms when plots merge as it's likely more will be removed than will need to be created
+    // again (by not testing for base plot etc)
+    @Subscribe
+    public void onPlotMerge(PlotMergeEvent e) {
+        UUID owner = e.getPlot().getOwnerAbs();
+        if (owner == null) {
+            return;
+        }
+        HoloPlotID id = new HoloPlotID(e.getPlot().getId(), owner);
+        Hologram hologram = PSHoloUtil.holograms.remove(id);
+        if (hologram != null) {
+            hologram.delete();
         }
     }
 
