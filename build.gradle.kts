a1c3f0c6df8ff8d53274b119db71f373e6a33992
NotMyFault <mc.cache@web.de>
2021-12-14 23:35:32 +0100

    build: Back to snapshot for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ the<JavaPluginExtension>().toolchain {
     languageVersion.set(JavaLanguageVersion.of(16))
 }
 
-version = "6.2.1"
+version = "6.2.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
