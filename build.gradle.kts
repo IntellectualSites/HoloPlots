bc03f74d09bec940b975ce97b7ac830eefcd74bc
Alexander Brandes <mc.cache@web.de>
2023-03-02 10:03:24 +0100

    Update PaperMC repository URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,8 @@ version = "6.2.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
     maven { url = uri("https://repo.codemc.io/repository/maven-public/") }
-    maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
     maven { url = uri("https://repo.dmulloy2.net/nexus/repository/public/") }
     maven { url = uri("https://maven.enginehub.org/repo/") }
 }
