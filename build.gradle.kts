5d2adeec3007a4a575feed2ff364dd9d2c136403
Alexander Brandes <mc.cache@web.de>
2023-04-23 23:30:40 +0200

    Back to snapshot for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ configurations.all {
     attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
-version = "6.3.0"
+version = "6.3.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
