e8c5e78e4a1866f4e8db1579b89d6d63a9657918
Alexander Brandes <mc.cache@web.de>
2023-06-09 22:02:37 +0200

    Major version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ configurations.all {
     attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
-version = "6.3.1-SNAPSHOT"
+version = "7.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
