0638d2f965084fa34d6b9ea9640405693b45aeb7
Alexander Brandes <mc.cache@web.de>
2023-07-05 15:27:55 +0200

    Release 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ configurations.all {
     attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
-version = "7.0.0-SNAPSHOT"
+version = "7.0.0"
 
 repositories {
     mavenCentral()
