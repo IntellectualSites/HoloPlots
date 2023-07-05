77e27fcaf1f7006438a5243b30771af1e130fe65
Alexander Brandes <mc.cache@web.de>
2023-07-05 15:30:46 +0200

    Back to snapshot for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ configurations.all {
     attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
-version = "7.0.0"
+version = "7.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
