dd76c84bd671546f0a07b8267a44aff5a8f100b4
Alexander Brandes <mc.cache@web.de>
2024-02-05 23:19:50 +0100

    Back to snapshot for development
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ configurations.all {
     attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
-version = "7.1.0"
+version = "7.1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
