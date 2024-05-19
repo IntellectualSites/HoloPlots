9d7a1573a5826830183dcce69030a47182dd4283
Alexander Brandes <mc.cache@web.de>
2024-05-19 17:38:13 +0200

    Release 7.1.1
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ configurations.all {
     attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
 }
 
-version = "7.1.1-SNAPSHOT"
+version = "7.1.1"
 
 repositories {
     mavenCentral()
